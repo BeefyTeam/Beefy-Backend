b07a7c4234b02d64091aab530923e96764adde13
Jeremi Herodian Abednigo <Overload.emperor14@gmail.com>
2023-05-28 00:43:26 +0800

    Update jwt payload

--- BeefyREST/urls.py
+++ BeefyREST/urls.py
@@ -1,16 +1,27 @@
 from ninja import NinjaAPI
 from ninja import Router
-from BeefyREST.views import router
+from ninja.security import HttpBearer
+from BeefyREST.views import router, validTokenCheck
+
+
+class AuthBearer(HttpBearer):
+    def authenticate(self, request, token):
+        if (validTokenCheck(token=token)):
+            return token
+        else:
+            pass
+
 
 api = NinjaAPI(
     title='🥩 Beefy Endpoints',
     version='v1.0',
-    description='### Hello fellows what do you want? I serve anything for you in this documentation Beefy Restful Api',
-    default_router=Router(tags=['Root'])
+    description='### Hello fellows what do you want? Give me some meat, i will cook for you',
+    default_router=Router(tags=['Hello World'])
 )
 
 api.add_router('auth/', router)
 
-@api.get("/")
+
+@api.get("/", auth=AuthBearer())
 def hello(request):
     return {'message': "Hello I'm Beefy Backend"}
